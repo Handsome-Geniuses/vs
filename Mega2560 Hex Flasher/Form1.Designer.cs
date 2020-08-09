00ffda2f185b14029c5ea3b5454436d71cb264b0
johnson <j.n.le@outlook.com>
2020-08-08 19:21:22 -0700

    simple hex flasher for m2560

---
+++ Mega2560 Hex Flasher/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Mega2560_Hex_Flasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CB_Port_Selector = new System.Windows.Forms.ComboBox();
+            this.BTN_Browse = new System.Windows.Forms.Button();
+            this.OFD_FileOpen = new System.Windows.Forms.OpenFileDialog();
+            this.BTN_Begin_Flash = new System.Windows.Forms.Button();
+            this.cmd = new System.Diagnostics.Process();
+            this.button2 = new System.Windows.Forms.Button();
+            this.LB_Port = new System.Windows.Forms.Label();
+            this.TB_File_Path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TB_Msg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CB_Port_Selector
+            // 
+            this.CB_Port_Selector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_Port_Selector.FormattingEnabled = true;
+            this.CB_Port_Selector.Location = new System.Drawing.Point(35, 38);
+            this.CB_Port_Selector.Name = "CB_Port_Selector";
+            this.CB_Port_Selector.Size = new System.Drawing.Size(76, 21);
+            this.CB_Port_Selector.TabIndex = 0;
+            // 
+            // BTN_Browse
+            // 
+            this.BTN_Browse.Location = new System.Drawing.Point(224, 8);
+            this.BTN_Browse.Name = "BTN_Browse";
+            this.BTN_Browse.Size = new System.Drawing.Size(54, 23);
+            this.BTN_Browse.TabIndex = 2;
+            this.BTN_Browse.Text = "Browse";
+            this.BTN_Browse.UseVisualStyleBackColor = true;
+            this.BTN_Browse.Click += new System.EventHandler(this.BTN_Browse_Click);
+            // 
+            // OFD_FileOpen
+            // 
+            this.OFD_FileOpen.Filter = " HEX File (*.hex)|*.hex|All files (*.*)|*.*";
+            // 
+            // BTN_Begin_Flash
+            // 
+            this.BTN_Begin_Flash.Location = new System.Drawing.Point(117, 37);
+            this.BTN_Begin_Flash.Name = "BTN_Begin_Flash";
+            this.BTN_Begin_Flash.Size = new System.Drawing.Size(161, 23);
+            this.BTN_Begin_Flash.TabIndex = 4;
+            this.BTN_Begin_Flash.Text = "Flash";
+            this.BTN_Begin_Flash.UseVisualStyleBackColor = true;
+            this.BTN_Begin_Flash.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmd
+            // 
+            this.cmd.StartInfo.Domain = "";
+            this.cmd.StartInfo.LoadUserProfile = false;
+            this.cmd.StartInfo.Password = null;
+            this.cmd.StartInfo.StandardErrorEncoding = null;
+            this.cmd.StartInfo.StandardOutputEncoding = null;
+            this.cmd.StartInfo.UserName = "";
+            this.cmd.SynchronizingObject = this;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "s";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LB_Port
+            // 
+            this.LB_Port.BackColor = System.Drawing.SystemColors.Window;
+            this.LB_Port.Location = new System.Drawing.Point(3, 38);
+            this.LB_Port.Name = "LB_Port";
+            this.LB_Port.Size = new System.Drawing.Size(36, 21);
+            this.LB_Port.TabIndex = 6;
+            this.LB_Port.Text = "Port: ";
+            this.LB_Port.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TB_File_Path
+            // 
+            this.TB_File_Path.Location = new System.Drawing.Point(35, 10);
+            this.TB_File_Path.Name = "TB_File_Path";
+            this.TB_File_Path.Size = new System.Drawing.Size(183, 20);
+            this.TB_File_Path.TabIndex = 7;
+            this.TB_File_Path.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "File: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TB_Msg
+            // 
+            this.TB_Msg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TB_Msg.Location = new System.Drawing.Point(12, 65);
+            this.TB_Msg.Multiline = true;
+            this.TB_Msg.Name = "TB_Msg";
+            this.TB_Msg.ReadOnly = true;
+            this.TB_Msg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TB_Msg.Size = new System.Drawing.Size(266, 92);
+            this.TB_Msg.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(286, 164);
+            this.Controls.Add(this.TB_File_Path);
+            this.Controls.Add(this.CB_Port_Selector);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LB_Port);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.BTN_Begin_Flash);
+            this.Controls.Add(this.BTN_Browse);
+            this.Controls.Add(this.TB_Msg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JL UA Hex Flasher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox CB_Port_Selector;
+        private System.Windows.Forms.Button BTN_Browse;
+        private System.Windows.Forms.OpenFileDialog OFD_FileOpen;
+        private System.Windows.Forms.Button BTN_Begin_Flash;
+        private System.Diagnostics.Process cmd;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label LB_Port;
+        private System.Windows.Forms.TextBox TB_File_Path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TB_Msg;
+    }
+}
+
---
+++ Mega2560 Hex Flasher/Form1.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Mega2560_Hex_Flasher
+{
+    public partial class Form1 : Form
+    {
+        String cmdout="==================================================================================";
+        const String avr_no_connect = "ser_open(): can't open device";
+        const String avr_done = "Thank you.";
+        const String avr_bad_file = "ERROR: No valid record found in Intel Hex file";
+        const String avr_bad_dir = "can't open input file";
+        const String avr_connected = "AVR device initialized and ready to accept instructions";
+        const String avr_verifying = "verifying ...";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CB_Port_Selector.Items.AddRange(SerialPort.GetPortNames());
+            CheckForIllegalCrossThreadCalls = false;
+            OFD_FileOpen.InitialDirectory = Directory.GetCurrentDirectory();
+
+            //my cmd control options
+            cmd.StartInfo.CreateNoWindow = true;          
+            cmd.StartInfo.RedirectStandardInput = true;    
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.RedirectStandardError = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.EnableRaisingEvents = true;
+            cmd.StartInfo.FileName = "CMD.exe"; 
+            
+        }
+
+        private void BTN_Browse_Click(object sender, EventArgs e)
+        {
+            if (OFD_FileOpen.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //adding quotes to filename just in case there are spaces.
+                String strfilename = OFD_FileOpen.FileName;
+                strfilename = strfilename.Replace(@"\", "/");
+                TB_File_Path.Text = "\""+strfilename+ "\"";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //check if path selected
+            if (TB_File_Path.Text == "")
+            {
+                TB_Msg.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + "No file selected!" + Environment.NewLine);
+                return;
+            }
+            //check if port selected
+            if (CB_Port_Selector.Text == "")
+            {
+                //please select com
+                TB_Msg.AppendText(DateTime.Now.ToString("[HH:mm:ss] ")+"No COM port selected!"+Environment.NewLine);
+                return;
+            }
+            //command for cmd
+            String command = "jl_flasher -Cjl_flasher.conf -cwiring -pm2560 -q -D -Uflash:w:"+ TB_File_Path.Text+":i -P"+ CB_Port_Selector.Text+" & exit";
+            String temp = "";
+
+            //start flash msg
+            TB_Msg.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + "Please don't disconnect device." + Environment.NewLine);
+            cmd.Start();
+            cmd.StandardInput.WriteLine(command);
+
+
+            //check progress and errors
+            temp = cmd.StandardError.ReadToEnd();
+            cmdout += temp;
+            if (temp.Contains(avr_no_connect))
+            {
+                TB_Msg.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + "ERROR: Could not connect to device." + Environment.NewLine);
+            }
+            if (temp.Contains(avr_bad_file))
+            {
+                TB_Msg.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + "ERROR: Please check firmware file." + Environment.NewLine);
+            }
+            if (temp.Contains(avr_bad_dir))
+            {
+                TB_Msg.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + "ERROR: Can't find firmware file." + Environment.NewLine);
+            }
+            if (temp.Contains(avr_verifying))
+            {
+                TB_Msg.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + "Upload success. Flash Data Verified." + Environment.NewLine);
+            }
+            if (temp.Contains(avr_done))
+            {
+                TB_Msg.AppendText(DateTime.Now.ToString("[HH:mm:ss] ") + "Finished. You may disconect." + Environment.NewLine);
+            }
+            cmd.WaitForExit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.Write(cmdout);
+            System.Diagnostics.Debug.Write("==================================================================================");
+        }
+    }
+}
